68b064b62ea4b32ec0e8273a36aa2ea7361b128f
Aditya <adityaramesh@lishash.com>
2021-05-19 13:29:55 -0400

    removed minimizer

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,6 +3,5 @@ from lib.api import *
 from lib.graph import *
 from lib.logger import *
 from lib.manipulation import *
-from lib.minimizer import *
 from lib.pre_processor import *
 from lib.c_elegans import *
